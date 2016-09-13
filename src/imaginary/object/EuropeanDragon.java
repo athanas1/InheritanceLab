fab101d2765e74496dc426316aae2c31eb8c36df
Alex <Alex@Alex-PC.attlocal.net>
2016-09-12 20:22:38 -0500

    3rd Made dragon abstractions

--- src/imaginary/object/EuropeanDragon.java
+++ src/imaginary/object/EuropeanDragon.java
@@ -9,13 +9,8 @@ package imaginary.object;
  *
  * @author Alex
  */
-public class InheritanceLab {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class EuropeanDragon {
+ 
     
 }
+
