a54bb433e4fee671708619fcfbf5b12348648334
Alex <Alex@Alex-PC.attlocal.net>
2016-09-12 19:53:07 -0500

    2nd commit finished first imaginary being

--- src/imaginary/object2/HarryPotter.java
+++ src/imaginary/object2/HarryPotter.java
@@ -10,7 +10,7 @@ package imaginary.object2;
  * @author Alex
  */
 public class HarryPotter extends HogwartsStudent {
-    
+    // only HarryPitter can kill Lord Voldemort
     public void fightDarkLord(){
         System.out.println("I Killed Voldemort");
     }
